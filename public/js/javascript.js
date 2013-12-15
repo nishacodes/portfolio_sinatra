dedef52eff0626502353f3d8f0eea86eb4b23dce
nishacodes <nishabatra27@gmail.com>
2013-12-15 15:37:21 -0500

    add fadein navbar using js and link to sections

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,12 +1,19 @@
-$(window).on('scroll', function() {
-    console.log($(this).scrollTop());
-    if ($(this).scrollTop() > 150 && !$('.nav').hasClass('visible')) {
-        $('.nav').animate({opacity : 1}, 'slow', function() {
-            $(this).addClass('visible').removeAttr('style');
+$(function(){
+  
+  var menu = $('#menu'),
+    pos = menu.offset();
+    
+    $(window).scroll(function(){
+      if($(this).scrollTop() > pos.top+menu.height() && menu.hasClass('default') && $(this).scrollTop() > 500){
+        menu.fadeOut('fast', function(){
+          $(this).removeClass('default').addClass('fixed').fadeIn('fast');
         });
-    } else if ($(this).scrollTop() <= 150 && $('.nav').hasClass('visible')) {
-        $('.nav').animate({opacity : 0}, 'slow', function() {
-            $(this).removeClass('visible').removeAttr('style');
+      } else if($(this).scrollTop() <= pos.top+500 && menu.hasClass('fixed')){
+        menu.fadeOut('fast', function(){
+          $(this).removeClass('fixed').addClass('default').fadeOut('fast');
+                    console.log($(this).html());
         });
-    }
+      }
+    });
+
 });
