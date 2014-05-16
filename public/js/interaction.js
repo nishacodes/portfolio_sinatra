f9d332bdcdc52443a15f46c624c316422f9047bc
nishacodes <nishabatra27@gmail.com>
2014-05-15 21:49:49 -0400

    style project displays

--- public/js/interaction.js
+++ public/js/interaction.js
@@ -20,14 +20,14 @@ $(document).ready(function(){
   $(".overlay").fadeOut(1500, function(){
     $("h1").fadeIn(1000, function(){
       $("h2").typed({
-        strings: ["developer.", "designer.", "dreamer.", "developer. designer. dreamer. "],
-        typeSpeed: 30,
+        strings: ["developer. ^400 designer. ^400 dreamer."],
+        typeSpeed: 20,
         backDelay: 700, // pause before backspacing
         backSpeed: 30
       });
     });
   });
-  $(".button-round").delay(8000).fadeIn(1000);
+  $(".button-round").delay(5500).fadeIn(1000);
 
   // SMOOTH SCROLL
   $('a[href*=#]:not([href=#])').click(function() {
@@ -46,22 +46,22 @@ $(document).ready(function(){
   });
 
   // STICKY NAV
-  $(function(){
-    var menu = $('#menu'),
-      pos = menu.offset();
-      $(window).scroll(function(){
-        if($(this).scrollTop() > pos.top+menu.height() && menu.hasClass('default') && $(this).scrollTop() > 500){
-          menu.fadeOut('fast', function(){
-            $(this).removeClass('default').addClass('fixed').fadeIn('fast');
-          });
-        } else if($(this).scrollTop() <= pos.top+500 && menu.hasClass('fixed')){
-          menu.fadeOut('fast', function(){
-            $(this).removeClass('fixed').addClass('default').fadeOut('fast');
-                      console.log($(this).html());
-          });
-        }
-      });
-  });
+  // $(function(){
+  //   var menu = $('#menu'),
+  //     pos = menu.offset();
+  //     $(window).scroll(function(){
+  //       if($(this).scrollTop() > pos.top+menu.height() && menu.hasClass('default') && $(this).scrollTop() > 500){
+  //         menu.fadeOut('fast', function(){
+  //           $(this).removeClass('default').addClass('fixed').fadeIn('fast');
+  //         });
+  //       } else if($(this).scrollTop() <= pos.top+500 && menu.hasClass('fixed')){
+  //         menu.fadeOut('fast', function(){
+  //           $(this).removeClass('fixed').addClass('default').fadeOut('fast');
+  //                     console.log($(this).html());
+  //         });
+  //       }
+  //     });
+  // });
 
   // Carousel
   // $('.carousel').carousel();
@@ -80,21 +80,23 @@ $(document).ready(function(){
     }
     // change size of item via class
     classie.toggle( event.target, 'gigante' );
+    $(this).find('.content').toggle();
     // trigger layout
     msnry.layout();
   });
 
-  $(".item").mouseover(function(){
+  // ITEM ANIMATION
+  $(".item").on({
+    'mouseover': function(){
       $(this).find(".top").fadeOut(200);  
-  })
-
-  $(".item").mouseleave(function(){
-    if($(this).hasClass('gigante')){
-      // 
-    }else {
-      $(this).find(".top").fadeIn(500);
+    },
+    'mouseleave': function(){
+      if($(this).hasClass('gigante')){
+      }else {
+        $(this).find(".top").fadeIn(500);
+      }
     }
-  })
+  });
 
   $(".middle").click(function(){
     $(this).hide();
@@ -106,8 +108,69 @@ $(document).ready(function(){
     $(this) .parent().trigger( "click" );
   })
 
+  // HI
+  $(".hi").mouseover(function(){
+    $(this).find("h3.type").typed({
+        strings: ["I'm Nisha"],
+        typeSpeed: 30,
+        backDelay: 700, // pause before backspacing
+        backSpeed: 30
+      });
+  })
+
+  // NYC
+  $('.nyc').on({
+    'mouseenter': function(){
+      $(this).animate({
+        'background-position-x': '-100px',
+        'background-position-y': '-10px'
+      }, 6500, 'linear'); 
+    },
+    'mouseleave': function(){
+      $(this).stop();
+    }
+  });
+
+  $('.localist').on({
+    'mouseenter': function(){
+      // $(this).find('.top, .middle').animate({
+      //   'background-position-x': '0px',
+      //   'background-position-y': '0px'
+      // }, 2000); 
+    },
+    'mouseleave': function(){
+      // $(this).find('.top, .middle').stop();
+    }
+  });
+
+  // Stacking blocks
+  $(".code").on({
+    'mouseover':function(){
+      $(".rails").slideDown(300, function(){
+        $(".ruby").slideDown(300, function(){
+          $(".sinatra").slideDown(300, function(){
+            $(".javascript").slideDown(300, function(){
+              $(".jquery").slideDown(300, function(){
+                $(".d3").slideDown(300, function(){
+                  $(".haml, .sass").slideDown(300);
+                })  
+              })
+            })
+          })  
+        })
+      })
+    },
+    'mouseleave':function(){
+      $(".block").fadeOut();
+    }
+  })
+
+
+
 
 })
+
+
 
 
 
